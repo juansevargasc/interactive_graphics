5f2d7ffd6ba67766351bc8854ac1ed5df460f8b4
Juanse Vargas MBP16 <jsvargasc@unal.edu.co>
2024-05-27 17:56:21 -0500

    :art: Improvements

--- Ejercicio_3_Final/mapa.js
+++ Ejercicio_3_Final/mapa.js
@@ -1,14 +1,15 @@
 // Espera a que el contenido del DOM se haya cargado completamente
 document.addEventListener("DOMContentLoaded", function() {
-    // Inicializa el mapa y establece la vista inicial en el centro de Colombia con un nivel de zoom de 6
+    // L viene de la librería extenal Leafleet importada en mapa.html
+    // Acá se inicializa el mapa y establece la vista inicial en el centro de Colombia con un nivel de zoom de 6
     var mapa = L.map('mapa').setView([4.5709, -74.2973], 6); // Longitud, latitud, zoom.
 
     // Agrega una capa de mosaicos llamdaos tiles de OpenStreetMap al mapa
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap Créditos para el Ejercicio Final. Gráfica Interactiva - UNAL' // Atribución para OpenStreetMap
+        attribution: '© OpenStreetMap Créditos para el Ejercicio Final. Gráfica Interactiva - UNAL  ' // Atribución para OpenStreetMap
     }).addTo(mapa);
 
-    // Define las ubicaciones de los concesionarios de autos
+    // Define las ubicaciones de los concesionarios de autos:
     var concesionarios = [
         { "marca": "BMW", "ubicacion": [4.7110, -74.0721], "informacion": "Concesionario BMW Colombia", "url": "https://www.bmw.com.co", "ciudad": "Bogotá" },
         { "marca": "Mercedes", "ubicacion": [4.6097, -74.0817], "informacion": "Concesionario Mercedes Colombia", "url": "https://www.mercedes-benz.com.co", "ciudad": "Bogotá" },
@@ -18,11 +19,13 @@ document.addEventListener("DOMContentLoaded", function() {
         { "marca": "Hyundai", "ubicacion": [7.1254, -73.1198], "informacion": "Concesionario Hyundai Colombia", "url": "https://www.hyundai.com.co", "ciudad": "Bucaramanga" }
     ];
 
-    // Recorre cada concesionario y agrega un marcador en el mapa
+    // Recorre objeto del diccionario concesionarios. A cada uno le asigna una ubicación en el mapa.
+    // Usa popup para el punto en el mapa.
     concesionarios.forEach(function(concesionario) {
         // Agrega un marcador en la ubicación del concesionario
         L.marker(concesionario.ubicacion).addTo(mapa)
             // Agrega un popup con información del concesionario
+            // Target blank hace que se abra una pestaña aparte
             .bindPopup('<b>' + concesionario.marca + '</b><br>' + concesionario.ciudad + '<br><a href="' + concesionario.url + '" target="_blank">' + concesionario.informacion + '</a>')
             // Abre el popup por defecto
             .openPopup();
