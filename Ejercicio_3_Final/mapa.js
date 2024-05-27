233d6fafc0a291edeeece57884c97ae6463f874a
Juanse Vargas MBP16 <jsvargasc@unal.edu.co>
2024-05-27 17:33:37 -0500

    :zap: FINAL exercise

---
+++ Ejercicio_3_Final/mapa.js
@@ -0,0 +1,30 @@
+// Espera a que el contenido del DOM se haya cargado completamente
+document.addEventListener("DOMContentLoaded", function() {
+    // Inicializa el mapa y establece la vista inicial en el centro de Colombia con un nivel de zoom de 6
+    var mapa = L.map('mapa').setView([4.5709, -74.2973], 6); // Longitud, latitud, zoom.
+
+    // Agrega una capa de mosaicos llamdaos tiles de OpenStreetMap al mapa
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap Créditos para el Ejercicio Final. Gráfica Interactiva - UNAL' // Atribución para OpenStreetMap
+    }).addTo(mapa);
+
+    // Define las ubicaciones de los concesionarios de autos
+    var concesionarios = [
+        { "marca": "BMW", "ubicacion": [4.7110, -74.0721], "informacion": "Concesionario BMW Colombia", "url": "https://www.bmw.com.co", "ciudad": "Bogotá" },
+        { "marca": "Mercedes", "ubicacion": [4.6097, -74.0817], "informacion": "Concesionario Mercedes Colombia", "url": "https://www.mercedes-benz.com.co", "ciudad": "Bogotá" },
+        { "marca": "Audi", "ubicacion": [6.2518, -75.5636], "informacion": "Concesionario Audi Colombia", "url": "https://www.audi.com.co", "ciudad": "Medellín" },
+        { "marca": "Volkswagen", "ubicacion": [3.4516, -76.5319], "informacion": "Concesionario Volkswagen Colombia", "url": "https://www.volkswagen.com.co", "ciudad": "Cali" },
+        { "marca": "Ford", "ubicacion": [10.9878, -74.7889], "informacion": "Concesionario Ford Colombia", "url": "https://www.ford.com.co", "ciudad": "Barranquilla" },
+        { "marca": "Hyundai", "ubicacion": [7.1254, -73.1198], "informacion": "Concesionario Hyundai Colombia", "url": "https://www.hyundai.com.co", "ciudad": "Bucaramanga" }
+    ];
+
+    // Recorre cada concesionario y agrega un marcador en el mapa
+    concesionarios.forEach(function(concesionario) {
+        // Agrega un marcador en la ubicación del concesionario
+        L.marker(concesionario.ubicacion).addTo(mapa)
+            // Agrega un popup con información del concesionario
+            .bindPopup('<b>' + concesionario.marca + '</b><br>' + concesionario.ciudad + '<br><a href="' + concesionario.url + '" target="_blank">' + concesionario.informacion + '</a>')
+            // Abre el popup por defecto
+            .openPopup();
+    });
+});
